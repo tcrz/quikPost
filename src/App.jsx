da659a755ceddb30a5a645262dcba9e13b19edaf
“tcrz” <tonybaidoo0@gmail.com>
2022-12-28 23:23:36 +0000

    update default post

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const welcomePost = {
   id: nanoid(),
   title: "⚡Welcome to QuikPost",
   content:
-    "QuikPost is a post webapp that allows users to create, edit and delete posts. Posts are stored in local storage. Built with React.\nFeatures: \n- Add/Create a post\n- Edit posts\n- Delete posts\n\nPossible Updates:\n- Pagination to prevent all posts from being on a single page.\n- Import and export(download) of posts as JSON file.\n- Special styling/effect can be added to a newly added post.\n\nGithub repo: https://github.com/tcrz/quikPost",
+    "QuikPost is a post webapp that allows users to create, edit and delete posts. Posts are stored in local storage. Built with React.\nFeatures: \n- Add/Create a post\n- Edit posts\n- Delete posts\n\nPossible Updates:\n- Pagination to prevent all posts from being on a single page.\n- Import and export(download) of posts as JSON file.\n- Special styling/effect can be added to a newly added post.",
   date: new Date(2022, 11, 26, 20, 19),
 };
 
