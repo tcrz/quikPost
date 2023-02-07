3d6fc27525bc82c800492b63f404b7fd52861fe1
“tcrz” <tonybaidoo0@gmail.com>
2023-02-07 11:50:50 +0000

    Implement download feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Posts from "./components/Posts/Posts";
 import PostView from "./components/PostView/PostView";
-import { nanoid } from "nanoid";
 import MainHeader from "./components/MainHeader/MainHeader";
-import emptyboximg from "./assets/emptybox.png";
-import { Button } from "semantic-ui-react";
+
+import { Button, Icon } from "semantic-ui-react";
+import DownloadButton from "./components/DownloadButton/DownloadButton";
+import EmptyPosts from "./components/EmptyPosts/EmptyPosts";
 
 const welcomePost = {
-  id:  "quikpost101",
+  id: "quikpost101",
   title: "⚡Welcome to QuikPost",
   content:
     "QuikPost is a post webapp that allows users to create, edit and delete posts. Posts are stored in local storage. Built with React.\nFeatures: \n- Add/Create a post\n- Edit posts\n- Delete posts\n\nPossible Updates:\n- Pagination to prevent all posts from being on a single page.\n- Import and export(download) of posts as JSON file.\n- Special styling/effect can be added to a newly added post.",
   date: new Date(2022, 11, 26, 20, 19),
+};
+
+const downloadData = (data, fileName) => {
+  const blob = new Blob([JSON.stringify(data)], { type: "type/json" });
+  a.download = fileName;
+  const link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = fileName;
+
+  const clickEvent = new MouseEvent("click", {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+  });
+  link.dispatchEvent(clickEvent);
 };
 
 function App() {
@@ -26,6 +42,9 @@ function App() {
   }
 
   const [posts, setPosts] = useState(allPosts || [welcomePost]);
+  const blob = new Blob([JSON.stringify(posts)], { type: "type/json" });
+  const url = window.URL.createObjectURL(blob);
+  const fileName = "posts.json";
 
   useEffect(() => {
     localStorage.setItem("posts", JSON.stringify(posts));
@@ -48,29 +67,34 @@ function App() {
     setPosts(postsCopy);
   };
 
+  const DownloadBtn = () => (
+    <Button size="tiny" className="download-btn">
+      <Icon name="download" />
+      <a href={url} download={fileName}>
+        Download posts data
+      </a>
+    </Button>
+  );
   return (
     <>
       <div className="App">
         <Layout>
           <MainHeader addPost={addPost} />
-          {posts.length === 0 && (
-            <div className="empty-section">
-              <img src={emptyboximg} alt="empty box image" />
-              <p>
-                Looks like you have not posted anything.
-                <br /> Add a new post? :)
-              </p>
-            </div>
-          )}
           <Routes>
             <Route
               path="/home"
               element={
-                <Posts
-                  editPost={editPost}
-                  deletePost={deletePost}
-                  posts={posts}
-                />
+                posts.length === 0 ? (
+                  <EmptyPosts/>
+                ) : (
+                  <Posts
+                    editPost={editPost}
+                    deletePost={deletePost}
+                    posts={posts}
+                  >
+                    <DownloadButton url={url} fileName={fileName} />
+                  </Posts>
+                )
               }
             />
             <Route path="/post/:id" element={<PostView />} />
