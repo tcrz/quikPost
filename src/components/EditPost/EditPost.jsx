d80522aaaa4ed9b414ac642a6547c37b94fc66e1
“tcrz” <tonybaidoo0@gmail.com>
2023-02-07 08:42:12 +0000

    modal shorthand alternative

--- src/components/EditPost/EditPost.jsx
+++ src/components/EditPost/EditPost.jsx
@@ -7,6 +7,23 @@ export default function EditPost(props) {
   const [open, setOpen] = useState(false);
 
   return (
+    //modal shorthand
+    // <Modal
+    //   size="small"
+    //   open={open}
+    //   setOpen={setOpen}
+    //   onOpen={() => setOpen(true)}
+    //   onClose={() => setOpen(false)}
+    //   trigger={props.trigger}
+    //   header="Edit Post"
+    //   content={{children:<PostForm
+    //       {...props}
+    //       setOpen={setOpen}
+    //       editPost={props.editPost}
+    //       buttonLabel={"Update"}
+    //     />}}
+    // />
+
     <ModalWindow
       size={"small"}
       open={open}
