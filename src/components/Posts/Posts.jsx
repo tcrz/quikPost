bfbfcae206b4a092834efacd834bce1f79859f18
“tcrz” <tonybaidoo0@gmail.com>
2023-02-07 11:51:19 +0000

    code/style enhancements

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -5,17 +5,20 @@ import "./Posts.css";
 export default function Posts(props) {
   return (
     <div className="posts-section">
-      {props.posts.map((post) => (
-        <PostItem
-          id={post.id}
-          key={post.id}
-          title={post.title}
-          content={post.content}
-          date={post.date}
-          deletePost={props.deletePost.bind(null, post.id)}
-          editPost={props.editPost.bind(null, post.id)}
-        />
-      ))}
+      <div className="posts">
+        {props.posts.map((post) => (
+          <PostItem
+            id={post.id}
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            date={post.date}
+            deletePost={props.deletePost.bind(null, post.id)}
+            editPost={props.editPost.bind(null, post.id)}
+          />
+        ))}
+      </div>
+      {props.children}
     </div>
   );
 }
